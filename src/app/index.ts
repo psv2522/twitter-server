b6dab8db46d8c4e541bef21b081b10a8110b6961
Prathmesh Vhatkar <vhatkarps20.comp@coep.ac.in>
2024-08-25 19:14:10 +0530

    user config added

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,21 +2,23 @@ import express from "express"
 import { ApolloServer } from "@apollo/server"
 import bodyParser from "body-parser";
 import { expressMiddleware } from "@apollo/server/express4"
+import { User } from "./user";
 
 export async function initServer() {
     const app = express();
 
     app.use(bodyParser.json());
-    
+
     const graphqlServer = new ApolloServer({
         typeDefs: `
+        ${User.types}
             type Query{
-                sayHello: String
+                ${User.queries}
             }
         `,
         resolvers: {
             Query: {
-                sayHello: () => `Hey from graphql from server`
+                ...User.resolvers.queries,
             },
         },
     });
