07fb853d014217ca383f037b432c00304beba987
Prathmesh Vhatkar <vhatkarps20.comp@coep.ac.in>
2024-08-25 19:48:28 +0530

    Making a jwt and saving the user in db

--- src/app/user/resolvers.ts
+++ src/app/user/resolvers.ts
@@ -1,6 +1,55 @@
+import axios from "axios"
+import { db } from "../../client/db";
+import JWTService from "../../services/jwt";
+
+interface GoogleTokenResult {
+    iss?: string;
+    azp?: string;
+    aud?: string;
+    sub?: string;
+    email?: string;
+    email_verified?: boolean;
+    at_hash?: string;
+    name?: string;
+    picture?: string;
+    given_name?: string;
+    family_name?: string;
+    locale?: string;
+    iat?: number;
+    exp?: number;
+    jti?: string;
+}
+
 const queries = {
     verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
-        return token;
+        const googleToken = token;
+        const googleOauthURL = new URL("https://oauth2.googleapis.com/tokeninfo");
+        googleOauthURL.searchParams.set("id_token", googleToken);
+
+        const { data } = await axios.get<GoogleTokenResult>(googleOauthURL.toString(), {
+            responseType: "json",
+        });
+
+        const user = await db.user.findUnique({ where: { email: data.email } });
+
+        if (!user) {
+            await db.user.create({
+                data: {
+                    email: data.email!,
+                    firstname: data.given_name!,
+                    lastname: data.family_name || "",
+                    profileimageURL: data.picture || ""
+                }
+            });
+
+        }
+        const userInDb = await db.user.findUnique({ where: { email: data.email } })
+
+        if (!userInDb) throw new Error("User with email not found")
+        const userToken = JWTService.generateTokenForUser(userInDb);
+
+        return userToken;
+
     },
 };
 
