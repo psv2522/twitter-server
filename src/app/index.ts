d1289d53de273b20a913df9f0c0c7bd36411367e
Prathmesh Vhatkar <vhatkarps20.comp@coep.ac.in>
2024-08-25 21:01:01 +0530

    Added cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,11 +3,13 @@ import { ApolloServer } from "@apollo/server"
 import bodyParser from "body-parser";
 import { expressMiddleware } from "@apollo/server/express4"
 import { User } from "./user";
+import cors from "cors";
 
 export async function initServer() {
     const app = express();
 
     app.use(bodyParser.json());
+    app.use(cors());
 
     const graphqlServer = new ApolloServer({
         typeDefs: `
